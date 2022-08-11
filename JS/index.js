3a50410c136d87623f6f819f5d44450732d25d78
Sanchez <dannysanchezgt@gmail.com>
2022-08-10 19:42:07 -0600

    Agregado de listas y parrafos html

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,69 @@
 /* Este es un comentario */
 
+var hola = "Hola Mundo";
+let hello = "Hello World";
+console.log(hola);
+console.log(hello);
+console.log(window.hola);
+console.log(window.hello);
+const PI = 3.1416;
+console.log(PI);
+let a;
+a = "kenai";
+console.log(a);
+const objeto = {
+  nombre: "jon",
+  edad: 35,
+};
 
-function holaMundo(nombre){
-    return `Hola ${nombre}`;
+const colores = ["blanco", "negro", "azul"];
+
+console.log(objeto);
+console.log(colores);
+
+objeto.correo = "jonmircha@gmail.com";
+colores.push("anaranjado");
+
+console.log(objeto);
+console.log(colores);
+
+let nombre = `Daniel`;
+console.log(nombre);
+let apellido = "Sanchez";
+let saludo = new String("Hola Mundo");
+let lorem = "lorem";
+
+console.log(nombre, apellido, saludo);
+console.log(nombre.length, apellido.length, saludo.length);
+
+//Funcion Declarada
+function estoEsUnaFuncion() {
+  console.log("Uno");
+  console.log("Dos");
+  console.log("Tres");
 }
+
+//Invocacion de funcion
+estoEsUnaFuncion();
+estoEsUnaFuncion();
+estoEsUnaFuncion();
+estoEsUnaFuncion();
+
+const san = {
+  nombre: "Dan",
+  apellido: "Sanchez",
+  edad: 35,
+  pasatiempos: ["correr", "nadar"],
+  saludar: function () {
+    console.log("Hola 😑");
+  },
+  decirMiNombre: function () {
+    console.log(
+      `Hola me llamo ${this.nombre} ${this.apellido} y tengo ${this.edad} años y estas sacando un curso`
+    );
+  },
+};
+
+console.log(san);
+console.log(san.decirMiNombre);
+san.decirMiNombre();
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-md
+# Aprendiendo _Markdown_
+
+Esto es un parrafo
+
+_Lorem ipsum_ dolor sit amet, consectetur adipisicing elit. Dicta fugit asperiores ab deleniti **necessitatibus** voluptatem sapiente animi aliquid ducimus delectus, perspiciatis saepe, autem, ad soluta! Consequuntur adipisci porro tempora quasi!.
+
+[Youtube](https://www.youtube.com/watch?v=FlsoBiteuPM&list=PLvq-jIkSeTUb9bRACHPvwzmh6_GMs5hAU&index=3)
+
+[Aprendiendo Markdown](#aprendiendo-markdown)
+
+[imagen]()
+
+---
+
+1. Primavera
+   - verano
+   - veterano 💕
+1. Verano
+1. Otoño
+
+- Invierno
+
+* Montaña
+
+> Siempre tienes opcion de no tener opinion. - Marco Aurelio
+
+> Todo lo que escuchamos es una opinión, no un hecho. Todo lo que vemos es una perspectiva, no la verdad.
+
+| nombre  | edad | Correo          |
+| ------- | ---- | --------------- |
+| Sanchez | 43   | pelon@gmail.com |
+
+para palabras `let` reservadas
+
+```js
+function sumar(a, b) {
+  return a + b;
+}
+```
